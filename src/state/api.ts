7de706870b006eb607d22e3e3f446c195ba8c54c
Newton Wakhungu <wakhungu22newton@gmail.com>
2024-10-15 23:15:45 +0300

    chore: set up the redux tool kit for state management

---
+++ src/state/api.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import build from "next/dist/build";
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+    reducerPath: "api",
+    tagTypes: [],
+    endpoints:(build) => ({}),
+});
+export const {} = api;
